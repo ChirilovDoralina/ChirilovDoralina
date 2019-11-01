6faf9c73a0b2a744b60414542fecb571d23fdf04
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 20:49:01 +0200

    Create T1_E5_200ms_ChirilovDoralina.m

---
+++ T1_E5_200ms_ChirilovDoralina.m
@@ -0,0 +1,12 @@
+t=0:0.2:12;
+%200ms
+x=1.5*abs(sin(0.5*pi*t));
+%->Vom folosi functia plot pentru a genera grafice cu parametrii x si y
+plot(t,x)
+%->Folosim functia axis pentru a delimita spectrele de valori ale graficului generat de plot pentru a se incadra corespunzator in parametri
+axis([0 12 -2 2])
+%Am denumit axa Ox
+xlabel('Timpul(secunde)')
+%Am denumit axa Oy
+ylabel('Amplitudinea semnalului')
+title('Semnal sinusoidal dublu redresat')
