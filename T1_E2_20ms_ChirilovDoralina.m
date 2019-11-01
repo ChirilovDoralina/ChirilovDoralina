55eb8d2ae347b9353892c227b74e11052c0e2c18
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 20:54:54 +0200

    Update T1_E2_20ms_ChirilovDoralina.m

--- T1_E2_20ms_ChirilovDoralina.m
+++ T1_E2_20ms_ChirilovDoralina.m
@@ -1,12 +1,12 @@
-t=0:.02:15;
+t=0:0.02:15;
 %3 perioade/20ms
 x=1.5*sawtooth(pi*0.4*t)-0.5;
 %->Vom atribui variabilei x o functie ce descrie un semnal triunghiular
 %->Vom folosi functia pot pentru a genera un grafic cu parametrii t si x
 plot(t,x)
-%->Vom folosi functia axis pentru a stabili intervalele de valori 
-%ale celor 2 axe
+%->Vom folosi functia axis pentru a stabili intervalele de valori ale celor 2 axe
 axis([0 15 -3 3])
+%->Acum graficul se va intinde de la 0 la 15 pe Ox si de a -3 la 3 pe Oy
 xlabel('Timpul(secunde)')
 %Am denumit axa Ox
 ylabel('Amplitudinea semnalului(um)')
