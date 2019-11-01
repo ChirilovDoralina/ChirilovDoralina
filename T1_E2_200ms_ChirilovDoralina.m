68070f340c6772f8f1cd61237bf8ad63ecf5228a
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 20:53:14 +0200

    Update T1_E2_200ms_ChirilovDoralina.m

--- T1_E2_200ms_ChirilovDoralina.m
+++ T1_E2_200ms_ChirilovDoralina.m
@@ -6,6 +6,7 @@ plot(t,x)
 %->Folosim functia plot pentru a genera un grafic cu parametrii t si x
 axis([0 15 -3 3])
 %->Folosim functia axis pentru a delimita limitele graficului generat de plot
+%->Acum graficul se va intinde de la 0 la 15 pe Ox si de a -3 la 3 pe Oy
 xlabel('Timpul(secunde)')
 %Am denumit axa Ox
 ylabel('Amplitudinea semnalului(um)')
