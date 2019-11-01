eeb08f40d7d2059e974713539a40214665874a12
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 21:01:49 +0200

    Update T1_E3a_20ms_ChirilovDoralina.m

--- T1_E3a_20ms_ChirilovDoralina.m
+++ T1_E3a_20ms_ChirilovDoralina.m
@@ -11,7 +11,8 @@ x1=r1*square(8*pi*t1,100);
 x2=-r2*square(8*pi*t2,100);
 %->Folosim functia axis pentru a delimita spectrele de valori ale graficului generat de plot pentru a se incadra corespunzator in parametri
 axis([0 1 -1 1]);
-%->Vom folosi functia pot pentru a genera un grafic cu parametrii t1 si x1 si un grafic cu t2 si x2
+%->Acum graficul se va intinde de la 0 la 1 pe Ox si de a -1 la 1 pe Oy
+%->Vom folosi functia plot pentru a genera un grafic cu parametrii t1 si x1 si un grafic cu t2 si x2
 plot(t1,x1);
 plot(t2,x2);
 t3=0.5:0.02:0.75;
@@ -19,7 +20,7 @@ t4=0.75:0.02:1;
 %Stabilesc nivelurile
 x3=r1*square(8*pi*t3,100);
 x4=-r2*square(8*pi*t4,100);
-%->Vom folosi functia pot pentru a genera un grafic cu parametrii t3 si x3 si un grafic cu t4 si x4
+%->Vom folosi functia plot pentru a genera un grafic cu parametrii t3 si x3 si un grafic cu t4 si x4
 plot(t3,x3);
 plot(t4,x4);
 %Am denumit axa Ox
