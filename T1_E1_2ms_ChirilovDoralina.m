add40ba4ae9b0f40fb1c07ca4d78919947e98652
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 20:54:03 +0200

    Update T1_E1_2ms_ChirilovDoralina.m

--- T1_E1_2ms_ChirilovDoralina.m
+++ T1_E1_2ms_ChirilovDoralina.m
@@ -4,8 +4,10 @@ x=0.75*square(pi*t,25)-0.25;
 %->Atribuim variabilei x functia unui semnal dreptunghiular
 %->Folosim functia plot pentru a genera un grafic cu parametrii t si x
 plot(t,x)
-%->Folosim functia axis pentru a delimita spectrele de valori ale graficului generat de plot pentru a se incadra corespunzator in parametri
+%->Folosim functia axis pentru a delimita spectrele de valori ale graficului generat de plot 
+%pentru a se incadra corespunzator in parametri
 axis([0 8 -2 2])
+%->Acum graficul se va intinde de la 0 la 8 pe Ox si de a -2 la 2 pe Oy
 %->Am denumit axa Ox
 xlabel('Timpul(secunde)')
 %->Am denumit axa Oy
