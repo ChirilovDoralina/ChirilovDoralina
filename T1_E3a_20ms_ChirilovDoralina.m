642b35f0336c8fde822f6a6f1cdb5b8095245547
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 19:25:04 +0200

    Update T1_E3a_20ms_ChirilovDoralina.m

--- T1_E3a_20ms_ChirilovDoralina.m
+++ T1_E3a_20ms_ChirilovDoralina.m
@@ -6,6 +6,7 @@ r1=rand(1);
 r2=rand(1);
 t1=0:0.02:0.25;
 t2=0.25:0.02:0.5;
+%Stabilesc nivelurile
 x1=r1*square(8*pi*t1,100);
 x2=-r2*square(8*pi*t2,100);
 %->Folosim functia axis pentru a delimita spectrele de valori ale graficului generat de plot pentru a se incadra corespunzator in parametri
@@ -15,6 +16,7 @@ plot(t1,x1);
 plot(t2,x2);
 t3=0.5:0.02:0.75;
 t4=0.75:0.02:1;
+%Stabilesc nivelurile
 x3=r1*square(8*pi*t3,100);
 x4=-r2*square(8*pi*t4,100);
 %->Vom folosi functia pot pentru a genera un grafic cu parametrii t3 si x3 si un grafic cu t4 si x4
