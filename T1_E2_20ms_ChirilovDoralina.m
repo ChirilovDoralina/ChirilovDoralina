169c1b9d1bcb08720582fd5fda60670072cc3c77
ChirilovDoralina <57265365+ChirilovDoralina@users.noreply.github.com>
2019-11-01 19:01:49 +0200

    Create T1_E2_20ms_ChirilovDoralina.m

---
+++ T1_E2_20ms_ChirilovDoralina.m
@@ -0,0 +1,14 @@
+t=0:.02:15;
+%3 perioade/20ms
+x=1.5*sawtooth(pi*0.4*t)-0.5;
+%->Vom atribui variabilei x o functie ce descrie un semnal triunghiular
+%->Vom folosi functia pot pentru a genera un grafic cu parametrii t si x
+plot(t,x)
+%->Vom folosi functia axis pentru a stabili intervalele de valori 
+%ale celor 2 axe
+axis([0 15 -3 3])
+xlabel('Timpul(secunde)')
+%Am denumit axa Ox
+ylabel('Amplitudinea semnalului(um)')
+%Am denumit axa Oy
+title('Semnal triunghiular periodic')
